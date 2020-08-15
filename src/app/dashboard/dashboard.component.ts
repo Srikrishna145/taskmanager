0c2f6d701ccc166054c8e28c9f57257948820c2c
unknown <srikrishnasahu.145@gmail.com>
2020-08-15 17:10:10 +0530

    PROJECT

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,6 +16,11 @@ ProjectCost: number;
 
 CurrentExpenditure: number;
 AvailableFunds: number;
+
+Clients: string[];
+Projects: string[];
+Years: string[];
+TeamMembersSummary: any[];
   ngOnInit(): void { 
     this.Designation="Team Leader";
     this.Username="Kanhu";
@@ -26,6 +31,49 @@ AvailableFunds: number;
     this.ProjectCost=2113507;
     this.CurrentExpenditure=96788;
     this.AvailableFunds=52536;
-  }
 
+    this.Clients=[
+      "ABC Infotech Ltd.","DEF Ltd.","GHI Software Ltd."
+    ];
+    this.Projects=[
+      "Project A","Project B","Project C","Project D"
+    ];
+    this.Years=[
+      "2018","2019","2020","2021"
+    ];
+    this.TeamMembersSummary=[
+     {Region:"East",TeamMembersCount:"50",UnavailableMembers:"20",AvailableMembers:"30"},
+     {Region:"West", TeamMembersCount:"40", UnavailableMembers:"20", AvailableMembers:"30"},
+     {Region:"North", TeamMembersCount:"50", UnavailableMembers:"20", AvailableMembers:"30"},
+     {Region:"South", TeamMembersCount:"50", UnavailableMembers:"20", AvailableMembers:"30"},
+     {Region:"North East", TeamMembersCount:"50", UnavailableMembers:"20", AvailableMembers:"30"},
+      
+    ];
+  }
+  onProjectChange($event)
+  {
+    if($event.target.innerHTML == "Project A"){
+      this.ProjectCost=2113507;
+    this.CurrentExpenditure=96788;
+    this.AvailableFunds=52536;
+    }
+    else if($event.target.innerHTML == "Project B")
+    {
+      this.ProjectCost=113507;
+    this.CurrentExpenditure=6788;
+    this.AvailableFunds=102536;
+    }
+    else if($event.target.innerHTML == "Project C")
+    {
+      this.ProjectCost=100;
+    this.CurrentExpenditure=200;
+    this.AvailableFunds=300;
+    }
+    else if($event.target.innerHTML == "Project D")
+    {
+      this.ProjectCost=507;
+    this.CurrentExpenditure=6788;
+    this.AvailableFunds=10253600;
+    }
+  }
 }
